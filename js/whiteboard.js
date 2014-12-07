62436d3a2af942dbec6b116381e37c88afbe6ef7
Garrett Finucane <garrettdreyfus@gmail.com>
2014-12-07 16:49:32 -0500

    added updatePlane

--- js/whiteboard.js
+++ js/whiteboard.js
@@ -1,15 +1,16 @@
-lastClick = new THREE.Vector3(0, 0, 0);
+var lastClick = new THREE.Vector3(0, 0, 0);
+var plane;
 function render () {
         requestAnimationFrame(render);
         controls.update();    
         renderer.render(scene, camera);
 }
-
+var rectMesh;
 function startWhiteBoardAnimation(){
     renderer = new THREE.WebGLRenderer({ antialiasing: true,alpha:true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1200);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000000);
     camera.position.set(0, 0, 1200);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     scene = new THREE.Scene();
@@ -22,22 +23,36 @@ function startWhiteBoardAnimation(){
     controls.target.set( 0, 0, 0 )
     render();
 }
+
+material = new THREE.LineBasicMaterial({
+        color: 0x000000
+});
+
 function addPoint(x,y,z){
-    var material = new THREE.LineBasicMaterial({
-            color: 0x000000
-    });
     var geometry = new THREE.Geometry();
     var vector = new THREE.Vector3(x,y,z);
-    var axis = new THREE.Vector3(1,0,0);
-    var angle = -(Math.PI /2);
-    vector.applyAxisAngle(axis,angle);
     geometry.vertices.push(vector);
     geometry.vertices.push(lastClick);
     var line = new THREE.Line(geometry, material);
     lastClick=vector;
     scene.add(line);
 }
+function updatePlane(rectLength,rectWidth,z){
+        if(rectMesh !== undefined){scene.remove(rectMesh)}
+        rectShape = new THREE.Shape();
+        rectShape.moveTo( 0,0 );
+        rectShape.lineTo( 0, rectWidth );
+        rectShape.lineTo( rectLength, rectWidth );
+        rectShape.lineTo( rectLength, 0 );
+        rectShape.lineTo( 0, 0 );
+        rectGeom = new THREE.ShapeGeometry( rectShape );
+        rectMesh = new THREE.Mesh( rectGeom, new THREE.MeshBasicMaterial( { color: 0x00FFFF,side:THREE.DoubleSide,transparent: true, opacity: 0.5} ) ) ;         
+        rectMesh.position.z = z;
+        scene.add( rectMesh );
+}
+
 function startWhiteBoard(){
-        startWhiteBoardAnimation();
+    startWhiteBoardAnimation();
+    updatePlane(100,100,100);
 }
 
